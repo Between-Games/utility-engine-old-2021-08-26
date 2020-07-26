38b8fbe74ba12f847bf2a1014d2f3f9bd9218ca0
S-A-R-C <s-a-r-c@s-a-r-c.com>
2020-07-26 15:44:46 -0400

    Configured Webpack to build when source file saved

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,13 @@ module.exports = {
     mode: 'production',
     entry: './source/index.js',
 
+    watch: true,
+    watchOptions: {
+        poll: 1000,
+        aggregateTimeout: 200,
+        ignored: '/node_modules/',
+    },
+
     output: {
         filename: 'index.js',
         library: 'utility-engine',
